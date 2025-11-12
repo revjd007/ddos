bb1ae4f660e389db1e42e12c9310939907debfca
revjd007 <pdigger48@gmail.com>
2025-11-12 17:31:14 -0600

    Delete login.js

--- login.js
+++
@@ -1,5 +0,0 @@
-const jwt = require('jsonwebtoken');
-module.exports = async (req, res) => {
-  // Find user, bcrypt.compare, if verified, jwt.sign({id: user.id})
-  res.json({ token: 'jwt...' });
-};
