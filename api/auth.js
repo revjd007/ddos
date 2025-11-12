9b6e70253803c0c4d840d18625d72b99774db33a
revjd007 <pdigger48@gmail.com>
2025-11-12 17:11:34 -0600

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,13 +1,3 @@
-const db = { users: {}, scripts: [] };
-
 module.exports = (req, res) => {
-  const { email, pass } = req.body;
-  if (!db.users[email]) {
-    db.users[email] = { pass, scripts: [] };
-    res.json({ token: 'fake-jwt-' + email });
-  } else if (db.users[email].pass === pass) {
-    res.json({ token: 'fake-jwt-' + email });
-  } else {
-    res.status(401).json({ error: 'Wrong password' });
-  }
+  res.json({ token: 'fake-jwt-' + req.body.email });
 };
